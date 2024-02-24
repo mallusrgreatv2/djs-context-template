eea190023aa1ee616871cb89d5f41d1dba159b30
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2024-02-24 23:53:40 +0530

    fix command showing in args

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,12 +1,15 @@
 import ClientEvent from "@/structures/event.js";
 import { Context } from "@mallusrgreat/djs-context";
-
 export default new ClientEvent({
   name: "messageCreate",
   async run(client, message) {
     if (!message.content.startsWith(client.config.PREFIX)) return;
     const args = message.content.trim().split(/ +/g);
-    const cmdName = args[0].slice(client.config.PREFIX.length).toLowerCase();
+    const cmdName = args
+      .shift()
+      ?.toLowerCase()
+      .substring(client.config.PREFIX.length);
+    console.log(cmdName);
     const command = client.commands.find((v) => v.data.name === cmdName);
     if (!command) return;
     const context = new Context(message);
